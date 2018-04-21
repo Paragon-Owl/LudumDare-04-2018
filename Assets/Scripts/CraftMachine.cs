df493a61a5505d37fd9e55073bb06d3705e93c69
Normand Benjamin <benjamin.normand@etu.u-bordeaux.fr>
2018-04-21 19:10:55 +0200

    CraftMachine

---
+++ Assets/Scripts/CraftMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftMachine : MonoBehaviour
+{
+    public GameObject Receipies;
+
+    public List<GameObject> getPossibleCrafts(Dictionary<GameObject,int> inventory)
+    {
+        List<GameObject> possibleCrafts = new List<GameObject>();
+        for(int i = 1; i<Receipies.GetComponentsInChildren<Transform>().Length; i++)
+        {
+            GameObject r = Receipies.GetComponentsInChildren<Transform>()[i].gameObject;
+            Receipe receipe = r.GetComponent<Receipe>();
+            if(receipe.canBeCraft(inventory))
+                possibleCrafts.Add(receipe.craftedObject);
+        }
+
+        return possibleCrafts;
+    }
+}
---
+++ Assets/Scripts/Receipe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receipe : MonoBehaviour
+{
+    [System.Serializable]
+    public class ReceipeContent
+    {
+        public GameObject item;
+        public int quantity;
+    }
+    public List<ReceipeContent> neededObjects;
+    public GameObject craftedObject;
+
+    public bool canBeCraft(Dictionary<GameObject, int> inventory)
+    {
+        foreach (ReceipeContent r in neededObjects)
+        {
+            if (!(inventory.ContainsKey(r.item) && inventory[r.item] > r.quantity))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

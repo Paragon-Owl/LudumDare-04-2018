19b89ba65a086542720a621fb381c798ac25772c
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-04-22 12:03:26 +0200

    tilemap and ui

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class ExtensionMethods {
+
+    public static T[] GetComponentsInDirectChildren<T>(this GameObject gameObject) {
+        int indexer = gameObject.transform.Cast<Transform>().Count(transform => transform.GetComponent<T>() != null);
+        var returnArray = new T[indexer];
+        indexer = 0;
+        
+        foreach (Transform transform in gameObject.transform) {
+            if (transform.GetComponent<T>() != null) {
+                returnArray[indexer++] = transform.GetComponent<T>();
+            }
+        }
+        return returnArray;
+    }
+}
---
+++ Assets/Scripts/GUI/ItemSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ItemSelector : MonoBehaviour {
+
+	public Color c;
+
+	private ArrayList myKeys;
+
+	private Image[] ItemsGUI;
+	// Use this for initialization
+	void Start () {
+		ItemsGUI = gameObject.GetComponentsInDirectChildren<Image>();
+		foreach (Image image in ItemsGUI) {
+			Debug.Log(image);
+		}
+		myKeys = new ArrayList {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = -1;
+		foreach(KeyCode key in myKeys) {
+			if (Input.GetKeyDown(key)) {
+				index = myKeys.IndexOf(key);
+			}
+		}
+		if (index != -1) {
+			foreach (Image image in ItemsGUI) {
+				image.color = Color.white;
+			}
+			ItemsGUI[index].color = c; //new Color(255 / 255.0f, 208 / 255.0f, 171 / 255.0f, 255 / 255.0f);
+		}
+	}
+}
